87edb2e1b3b16fb230a51b905505d4ec4126213b
tminus109 <martontibor123@gmail.com>
2023-09-26 20:01:30 +0200

    feat: recursion script

--- math.exs
+++ math.exs
@@ -1,11 +1,11 @@
 defmodule Math do
-    def sum(a, b) do
-       do_sum(a, b)
-    end
+  def sum(a, b) do
+    do_sum(a, b)
+  end
 
-    defp do_sum(a, b) do
-        a + b
-    end
+  defp do_sum(a, b) do
+    a + b
+  end
 end
 
 IO.puts Math.sum(1, 2)
